90eaa851c4b0fe40d56d4a355522d76218a65ade
ibrahim taofeek opeyemi <taofeekibrahimope@gmail.com>
2023-04-28 23:52:20 +0100

    completed the tutorial

--- src/learnings/donutChat/screens/Donut.tsx
+++ src/learnings/donutChat/screens/Donut.tsx
@@ -1,25 +1,108 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Svg, {G, Circle} from 'react-native-svg'
+import * as React from 'react';
+import { StyleSheet, Text, View, Animated, TextInput } from 'react-native';
+import Svg, { G, Circle } from 'react-native-svg';
 
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedInput = Animated.createAnimatedComponent(TextInput);
 
 const Donut = ({
-    percentage=75,
-    radius=40,
-    strokeWidth=10,
-    duration=500,
-    color='tomato',
-    delay=0,
-    textColor,
-    max=100
+  percentage = 70,
+  radius = 40,
+  strokeWidth = 10,
+  duration = 500,
+  color = 'tomato',
+  delay = 0,
+  textColor,
+  max = 100,
 }) => {
+  const animatedValue = React.useRef(new Animated.Value(0)).current;
+  const circleRef = React.useRef();
+  const inputRef = React.useRef();
+  const halfCircle = radius + strokeWidth;
+  const circleCircumference = 2 * Math.PI * radius;
+
+  const animation = (toValue: number) => {
+    // console.log(toValue)
+    return Animated.timing(animatedValue, {
+      toValue,
+      duration,
+      delay,
+      useNativeDriver: true,
+    }).start(() => {
+      animation(toValue === 0 ? percentage : 0);
+    });
+  };
+
+  React.useEffect(() => {
+    animation(percentage);
+
+    animatedValue.addListener((v) => {
+      if (circleRef?.current) {
+        const maxPerc = (100 * v.value) / max;
+        const strokeDashoffset =
+          circleCircumference - (circleCircumference * maxPerc) / 100;
+        circleRef.current.setNativeProps({
+          strokeDashoffset,
+        });
+      }
+      if (inputRef?.current) {
+        inputRef.current.setNativeProps({
+          text: `${Math.round(v.value)}`,
+        });
+      }
+    });
+
+    return () => {
+      animatedValue.removeAllListeners();
+    };
+  }, [max, percentage]);
   return (
     <View>
-      <Text>Donut:::</Text>
+      <Svg
+        width={radius * 2}
+        height={radius * 2}
+        viewBox={`0 0 ${halfCircle * 2} ${halfCircle * 2}`}
+      >
+        <G rotation='-90' origin={`${halfCircle}, ${halfCircle}`}>
+          <Circle
+            cx='50%'
+            cy='50%'
+            stroke={color}
+            strokeWidth={strokeWidth}
+            r={radius}
+            fill='transparent'
+            strokeOpacity={0.2}
+          />
+          <AnimatedCircle
+            ref={circleRef}
+            cx='50%'
+            cy='50%'
+            stroke={color}
+            strokeWidth={strokeWidth}
+            r={radius}
+            fill='transparent'
+            strokeDasharray={circleCircumference}
+            strokeDashoffset={circleCircumference}
+            strokeLinecap='round'
+          />
+        </G>
+      </Svg>
+
+      <AnimatedInput
+        ref={inputRef}
+        underlineColorAndroid='transparent'
+        editable={false}
+        defaultValue='0'
+        style={[
+          StyleSheet.absoluteFillObject,
+          { fontSize: radius / 2, color: textColor ?? color },
+          { fontWeight: '900', textAlign: 'center' },
+        ]}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Donut
+export default Donut;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/learnings/donutChat/screens/index.tsx
+++ src/learnings/donutChat/screens/index.tsx
@@ -1,15 +1,75 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 import React from 'react';
 import Donut from './Donut';
 
 const DonutChat = () => {
+  const data = [
+    {
+      percentage: 8,
+      color: 'tomato',
+      max: 10,
+      radius: 30,
+      strokeWidth: 20
+    },
+    {
+      percentage: 14,
+      color: 'skyblue',
+      max: 20,
+      radius: 80,
+      strokeWidth: 15
+    },   
+    {
+      percentage: 92,
+      color: 'gold',
+      radius: 60,
+      max: 100,
+    },
+    {
+      percentage: 240,
+      color: '#222',
+      max: 500,
+      radius: 100,
+    //   strokeWidth: 30
+    },
+  ];
+
   return (
-    <View>
-      <Donut />
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+        }}
+      >
+        {data.map((p, i) => {
+          return (
+            <Donut
+              key={i}
+              percentage={p.percentage}
+              color={p.color}
+              radius={p.radius}
+              delay={500 + 100 * i}
+              max={p.max}
+              strokeWidth={p.strokeWidth}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 };
 
 export default DonutChat;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+});
